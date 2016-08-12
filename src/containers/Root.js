4825e7efaa925b75bf0a7db36c1645b84bbfc086
Tuấn Vương <vanhtuan0409@gmail.com>
2016-08-12 13:30:04 +0700

    add react router

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import HomePage from '../layout/HomePage/HomePage.js';
 
 /**
@@ -8,6 +10,8 @@ import HomePage from '../layout/HomePage/HomePage.js';
 export default class Root extends Component {
   render() {
     const { store } = this.props;
+    const history = syncHistoryWithStore(browserHistory, store);
+
     return (
       /**
        * Provider is a component provided to us by the 'react-redux' bindings that
@@ -15,7 +19,9 @@ export default class Root extends Component {
        * calls in component hierarchy below.
        */
       <Provider store={store}>
-        <HomePage/>
+        <Router history={history}>
+          <Route path="/" component={HomePage}/>
+        </Router>
       </Provider>
     );
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
-
+import { routerReducer } from 'react-router-redux';
 /**
  * combineReducers is important to understand. As your app might grow in size
  * and complexity, you will likely begin to split your reducers into separate
@@ -15,7 +15,8 @@ import counter from './counter';
  * More info: http://rackt.org/redux/docs/api/combineReducers.html
  */
 const rootReducer = combineReducers({
-  counter, // you might be used to: counter: counter,
+  counter,
+  routing: routerReducer,
 });
 
 export default rootReducer;
