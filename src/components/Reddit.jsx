eb4cbcd91f11aa037241f379f047f73e15e8497f
tuan <vanhtuan0409@gmail.com>
2016-12-09 22:34:10 +0900

    separate file for container and components

---
+++ src/components/Reddit.jsx
@@ -0,0 +1,73 @@
+import React, { PureComponent, PropTypes } from 'react';
+import Picker from './Picker';
+import Posts from './Posts';
+
+export default class Reddit extends PureComponent {
+  static propTypes = {
+    selectedSubreddit: PropTypes.string.isRequired,
+
+    // Action
+    selectSubreddit: PropTypes.func.isRequired,
+    invalidateSubreddit: PropTypes.func.isRequired,
+    fetchPosts: PropTypes.func.isRequired,
+
+    // Post data
+    posts: PropTypes.arrayOf(PropTypes.object).isRequired,
+    isFetching: PropTypes.bool.isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+    this.handleRefreshClick = this.handleRefreshClick.bind(this);
+  }
+
+  componentDidMount() {
+    const { selectedSubreddit, fetchPosts } = this.props;
+    fetchPosts(selectedSubreddit);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.selectedSubreddit !== this.props.selectedSubreddit) {
+      const { selectedSubreddit, fetchPosts } = nextProps;
+      fetchPosts(selectedSubreddit);
+    }
+  }
+
+  handleRefreshClick(e) {
+    e.preventDefault();
+    const { selectedSubreddit, invalidateSubreddit, fetchPosts } = this.props;
+    invalidateSubreddit(selectedSubreddit);
+    fetchPosts(selectedSubreddit);
+  }
+
+  render() {
+    const { selectedSubreddit, posts, isFetching, selectSubreddit } = this.props;
+    return (
+      <div>
+        <Picker
+          value={selectedSubreddit}
+          onChange={selectSubreddit}
+          options={['reactjs', 'frontend']}
+        />
+        <p>
+          {!isFetching &&
+            <a href='' onClick={this.handleRefreshClick}>
+              Refresh
+            </a>
+          }
+        </p>
+        {isFetching && posts.length === 0 &&
+          <h2>Loading...</h2>
+        }
+        {!isFetching && posts.length === 0 &&
+          <h2>Empty.</h2>
+        }
+        {posts.length > 0 &&
+          <div style={{ opacity: isFetching ? 0.5 : 1 }}>
+            <Posts posts={posts} />
+          </div>
+        }
+      </div>
+    );
+  }
+};
--- src/containers/Reddit.jsx
+++ src/containers/Reddit.jsx
@@ -5,78 +5,7 @@ import {
 } from '../actions/redditActions';
 import { getPostBySubreddit, getPostBySubredditMeta } from '../redux/postBySubreddit';
 import { getSelectedSubreddit } from '../redux/selectedSubreddit';
-import Picker from '../components/Picker';
-import Posts from '../components/Posts';
-
-class RedditApp extends PureComponent {
-  static propTypes = {
-    selectedSubreddit: PropTypes.string.isRequired,
-
-    // Action
-    selectSubreddit: PropTypes.func.isRequired,
-    invalidateSubreddit: PropTypes.func.isRequired,
-    fetchPosts: PropTypes.func.isRequired,
-
-    // Post data
-    posts: PropTypes.arrayOf(PropTypes.object).isRequired,
-    isFetching: PropTypes.bool.isRequired,
-  }
-
-  constructor(props) {
-    super(props);
-    this.handleRefreshClick = this.handleRefreshClick.bind(this);
-  }
-
-  componentDidMount() {
-    const { selectedSubreddit, fetchPosts } = this.props;
-    fetchPosts(selectedSubreddit);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.selectedSubreddit !== this.props.selectedSubreddit) {
-      const { selectedSubreddit, fetchPosts } = nextProps;
-      fetchPosts(selectedSubreddit);
-    }
-  }
-
-  handleRefreshClick(e) {
-    e.preventDefault();
-    const { selectedSubreddit, invalidateSubreddit, fetchPosts } = this.props;
-    invalidateSubreddit(selectedSubreddit);
-    fetchPosts(selectedSubreddit);
-  }
-
-  render() {
-    const { selectedSubreddit, posts, isFetching, selectSubreddit } = this.props;
-    return (
-      <div>
-        <Picker
-          value={selectedSubreddit}
-          onChange={selectSubreddit}
-          options={['reactjs', 'frontend']}
-        />
-        <p>
-          {!isFetching &&
-            <a href='' onClick={this.handleRefreshClick}>
-              Refresh
-            </a>
-          }
-        </p>
-        {isFetching && posts.length === 0 &&
-          <h2>Loading...</h2>
-        }
-        {!isFetching && posts.length === 0 &&
-          <h2>Empty.</h2>
-        }
-        {posts.length > 0 &&
-          <div style={{ opacity: isFetching ? 0.5 : 1 }}>
-            <Posts posts={posts} />
-          </div>
-        }
-      </div>
-    );
-  }
-}
+import Reddit from '../components/Reddit';
 
 function mapStateToProps(state) {
   const selectedSubreddit = getSelectedSubreddit(state);
@@ -95,4 +24,4 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(RedditApp);
+export default connect(mapStateToProps, mapDispatchToProps)(Reddit);
