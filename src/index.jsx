597f81a8746f5883dea510f96b830e66b4248d10
tuan <vanhtuan0409@gmail.com>
2016-12-10 19:35:55 +0900

    refactor router and file location

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,11 @@ import ReactDOM from 'react-dom';
  * Both configureStore and Root are required conditionally.
  * See configureStore.js and Root.js for more details.
  */
-import { hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import configureStore from './store/configureStore';
-import Root from './containers/Root';
+import Router from './router';
 
 /**
  * Import the stylesheet you want used! Here we just reference
@@ -17,9 +18,11 @@ import Root from './containers/Root';
 import './styles/main.scss';
 
 const store = configureStore();
-const history = syncHistoryWithStore(hashHistory, store);
+const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
-  <Root store={store} history={history} />,
+  <Provider store={store}>
+    <Router history={history} />
+  </Provider>,
   document.getElementById('root') // eslint-disable-line
 );
---
+++ src/router.jsx
@@ -0,0 +1,34 @@
+import React, { PureComponent } from 'react';
+import { Router, Route, IndexRoute } from 'react-router';
+
+import App from './containers/App';
+import Reddit from './containers/Reddit';
+import FooView from './layout/FooView';
+import BarView from './layout/BarView';
+
+/**
+ * Component is exported for conditional usage in Root.js
+ */
+export default class Root extends PureComponent {
+  static propTypes = {
+    history: React.PropTypes.object,
+  }
+
+  render() {
+    const { history } = this.props;
+    return (
+      /**
+       * Provider is a component provided to us by the 'react-redux' bindings that
+       * wraps our app - thus making the Redux store/state available to our 'connect()'
+       * calls in component hierarchy below.
+       */
+      <Router history={history}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Reddit} />
+          <Route path="foo" component={FooView} />
+          <Route path="bar" component={BarView} />
+        </Route>
+      </Router>
+    );
+  }
+};
