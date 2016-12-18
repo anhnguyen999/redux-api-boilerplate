ee80e98b84b48f3ea49b77b8ec36ee833b3be42b
tuan <vanhtuan0409@gmail.com>
2016-12-18 21:51:03 +0900

    update travis script

--- src/layout/VersionView.jsx
+++ src/layout/VersionView.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function VersionView() {
   return (
     <div>
-      <div>Current version is 0.9</div>
+      <div>Current version is 0.9.0</div>
     </div>
   );
 };
