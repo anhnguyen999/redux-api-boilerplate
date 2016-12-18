9e692b51bb668f3a4eb1de2b887448e506eabf88
tuan <vanhtuan0409@gmail.com>
2016-12-18 19:38:21 +0900

    fix docker tag

--- scripts/deploy_docker.sh
+++ scripts/deploy_docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker login -u="$DOCKER_HUB_USER" -p="$DOCKER_HUB_PASS"
-docker tag $REPO/$APP_NAME:$TAG "$REPO/$APP_NAME:latest"
-docker push $REPO/$APP_NAME:$TAG
+docker tag $REPO/$APP_NAME "$REPO/$APP_NAME:$TRAVIS_TAG"
+docker push $REPO/$APP_NAME:$TRAVIS_TAG
+docker push $REPO/$APP_NAME
